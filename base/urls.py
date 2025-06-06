b474723182d2b002487e8a9b76bb072f0e64139b
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-06 23:09:14 +0530

    Added the basic form and submit button

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'), 
+
+    path('create_room/', views.createRoom, name='create-room'),
 ] 
 # Even if you change the path, no need to change at every place as already name="" variable helps in how to access and use the url tag in the home.html file
