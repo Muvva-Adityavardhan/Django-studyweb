87831dd28154e237c87e032161b96a187eb53d06
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-06 21:48:02 +0530

    Adding a new table in the DB

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    #host =
+    #topic =
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    #participants = 
+    updated = models.DateTimeField(auto_now=True) #auto_now --> Time stamp at every time we add 
+    created = models.DateTimeField(auto_now_add=True) #auto_now_add --> Time stamp at the time of creation only
+    def __str__(self):
+        return self.name
