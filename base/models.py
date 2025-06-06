854170f644d00c16f52b021da4635e4bcac37cf4
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-06 22:37:00 +0530

    Adding of all the part in models.py

--- base/models.py
+++ base/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+                            
+    def __str__(self):
+        return self.name
+
 class Room(models.Model):
-    #host =
-    #topic =
+    host = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic,on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     #participants = 
@@ -12,3 +18,13 @@ class Room(models.Model):
     created = models.DateTimeField(auto_now_add=True) #auto_now_add --> Time stamp at the time of creation only
     def __str__(self):
         return self.name
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room,on_delete=models.CASCADE) # SET_NULL --> If parent is deleted, then it will be set as Null, Cascade --> Delete
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True) #auto_now --> Time stamp at every time we add 
+    created = models.DateTimeField(auto_now_add=True) #auto_now_add --> Time stamp at the time of creation only
+
+    def __str__(self):
+        return self.body[0:50]
