8f24fc2ced5a6424c12076a24bd5b4f1fdf4efff
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-06 23:30:58 +0530

    The newest updated one comes on the top

--- base/models.py
+++ base/models.py
@@ -16,6 +16,10 @@ class Room(models.Model):
     #participants = 
     updated = models.DateTimeField(auto_now=True) #auto_now --> Time stamp at every time we add 
     created = models.DateTimeField(auto_now_add=True) #auto_now_add --> Time stamp at the time of creation only
+    
+    class Meta:
+        ordering = ['-updated', '-created']
+    
     def __str__(self):
         return self.name
     
