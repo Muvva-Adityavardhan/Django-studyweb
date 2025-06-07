fdf2b7dd19570fc281f7424c4fc8ca6f26e55ae4
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-07 22:32:13 +0530

    Delete a message

--- base/urls.py
+++ base/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('update_room/<str:pk>/', views.updateRoom, name='update-room'),
 
     path('delete_room/<str:pk>/', views.deleteRoom, name='delete-room'),
+    path('delete_message/<str:pk>/', views.deleteMessage, name='delete-message'),
 ] 
 # Even if you change the path, no need to change at every place as already name="" variable helps in how to access and use the url tag in the home.html file
