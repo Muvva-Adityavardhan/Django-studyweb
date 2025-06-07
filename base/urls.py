e0d5121dc794437e5322eb2ab5526664c11875dc
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-07 15:30:41 +0530

    Edit the form option

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name='room'), 
 
     path('create_room/', views.createRoom, name='create-room'),
+    path('update_room/<str:pk>/', views.updateRoom, name='update-room'),
 ] 
 # Even if you change the path, no need to change at every place as already name="" variable helps in how to access and use the url tag in the home.html file
