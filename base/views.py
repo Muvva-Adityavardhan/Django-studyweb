c769855f2e60823f1d1919bc418356c52a015434
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-07 23:17:28 +0530

    Completed the Activity profile section

--- base/views.py
+++ base/views.py
@@ -67,23 +67,28 @@ def registerPage(request):
 
 
 def home(request):
-    q = request.GET.get('q')
-    if q != None:
-        rooms = Room.objects.filter(
-            Q(topic__name__icontains=q) |
-            Q(name__icontains=q) |
-            Q(description__icontains=q)
-            ) 
-        #icontains in case insensitive, contains is case sensitive
-    else:
-        rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    # if q != None:
+    #     rooms = Room.objects.filter(
+    #         Q(topic__name__icontains=q) |
+    #         Q(name__icontains=q) |
+    #         Q(description__icontains=q)
+    #         ) 
+    #     #icontains in case insensitive, contains is case sensitive
+    # else:
+    #     rooms = Room.objects.all()
     
     #rooms = Room.objects.filter(topic__name=q)
 
     topics = Topic.objects.all()
     room_count = rooms.count()
     
-    room_messages = Message.objects.all()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     context = {'rooms':rooms, 'topics': topics, 'room_count':room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html', context)
