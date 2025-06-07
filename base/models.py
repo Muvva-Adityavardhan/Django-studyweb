52610d2c4200dc2beead6e85ac801d95e591c4af
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-07 22:46:51 +0530

    Addition of the activity feed component

--- base/models.py
+++ base/models.py
@@ -30,5 +30,8 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True) #auto_now --> Time stamp at every time we add 
     created = models.DateTimeField(auto_now_add=True) #auto_now_add --> Time stamp at the time of creation only
 
+    class Meta:
+        ordering = ['-updated', '-created']
+        
     def __str__(self):
         return self.body[0:50]
