9f69853154f262b94a9104cada585610bf1fa03d
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-08 10:09:22 +0530

    Fixing the createRoom form

--- base/forms.py
+++ base/forms.py
@@ -5,4 +5,5 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__' #Based on the attributes of the model in models.py file, all those values will be asked(editable values)
-        
+        exclude = ['host', 'participants'] #These values will not be asked in the form, as they are not editable by the user
+        # exclude = ['host'] #This will not ask the host value, as it is not editable by the user
