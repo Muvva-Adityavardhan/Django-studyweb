b2aa1179f1d0678d896e7b3772c3655f8529061a
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-08 18:51:54 +0530

    Working of the create room

--- base/views.py
+++ base/views.py
@@ -123,7 +123,7 @@ def userProfile(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
-
+    topics = Topic.objects.all()
     if(request.method == 'POST'):
         form = RoomForm(request.POST)
         if form.is_valid():
@@ -132,7 +132,7 @@ def createRoom(request):
             room.save()
             return redirect('home')
         
-    context = {'form':form}
+    context = {'form':form, 'topics':topics}
     return render(request, 'base/room_form.html',context)
 
 
@@ -140,7 +140,7 @@ def createRoom(request):
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-
+    topics = Topic.objects.all()
     if(request.user != room.host or request.user.is_superuser == False):
         return HttpResponse('You are not allowed here!!')
 
@@ -150,7 +150,7 @@ def updateRoom(request,pk):
             form.save()
             return redirect('home')
         
-    context = {'form':form}
+    context = {'form':form, 'topics':topics}
     return render(request, 'base/room_form.html',context)
 
 
